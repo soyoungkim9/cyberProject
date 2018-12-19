706313190fd2f3c2ba72842ae60f0fdccbba310b
soyoungkim9 <soyoung930623@gmail.com>
2018-12-19 11:01:58 +0900

    Week 3 Wed - node search success (JavaScript)

--- project1/src/main/webapp/js/search.js
+++ project1/src/main/webapp/js/search.js
@@ -4,23 +4,26 @@ $(function() {
 });
 
 var cnt;
-var name = "";
-var idSearch;
-var index// idSearch의 바로 밑 자식노드 위치
+var name;
+var idSearch; // 기준노드
+var index; // idSearch의 바로 밑 자식노드 위치
+var idValue;
 var nodeInput;
 var attrInput; 
 var attrValInput;
 
 function nodeSearch() {
+   event.preventDefault();
    cnt = 0;
    index = 0;
-   event.preventDefault();
+   name = "";
    // input 창에 작성한 값들 담기
+   idValue = document.getElementById('idValue').value;
    nodeInput = document.getElementById('nodeInput').value;
    attrInput = document.getElementById('attrInput').value;
    attrValInput = document.getElementById('attrValInput').value;
    // id input 창에 입력받은 id명 or 노드명 노드 찾기
-   idSearch = document.getElementById(document.getElementById('idValue').value);
+   idSearch = document.getElementById(idValue);
    
    var res;
    if(nodeInput =="" && attrInput =="" && attrValInput =="") {
@@ -54,49 +57,68 @@ function counting(node, preNode) {
 	  ++index; // idSearch의 바로 밑 자식노드를 이동시킨다.
     }
     // 2. 현재 node가 조건에 일치하는지 확인한다.
-    if(nodeInput == "" && node.attributes.length != 0) { // 노드명x 현재node의 속성이 존재하는 경우
+    if(nodeInput == "" && node.attributes.length != 0) { // 노드명(x) 
       for(var i = 0; i < node.attributes.length; i++) {
-    	 if(node.attributes[i].nodeName == attrInput) { /* id > 속성명 or 속성명 */
-    		name = "속성명 개수 = ";
-    		++cnt;
-    		console.log(node);
-    	 } else if(node.attributes[i].nodeValue == attrValInput) { /* id > 속성값 or 속성명 */
-    		name = "속성값 개수 = ";
-    		++cnt;
-    		console.log(node);
-    	 }
+    	if(idValue == "" && node.attributes[i].nodeName == attrInput &&
+   		   node.attributes[i].nodeValue == attrValInput) { 
+    		 name = "속성명 + 속성값 개수 = "; /* 노드명(x), id(x), 속성명 + 속성값 */
+   		     ++cnt;
+   		     console.log(node); 
+    	} else if(node.attributes[i].nodeName == attrInput && attrValInput == "") { 
+    	   if(idValue != "" && idValue == idSearch.id) {
+        	  name = "id > 속성명 = "; /* id > 속성명 */
+        	  ++cnt;
+        	  console.log(node);
+    	   } else if (idValue == ""){
+         	  name = "속성명 = "; /* 속성명 */
+        	  ++cnt;
+        	  console.log(node); 
+    	   }
+    	} else if(node.attributes[i].nodeValue == attrValInput && attrInput == "") { 
+     	   if(idValue != "" && idValue == idSearch.id) {
+       		  name = "id > 속성값 = "; /* id > 속성값 */
+    	      ++cnt;
+    	      console.log(node);
+           } else if (idValue == ""){
+              name = "속성값 = "; /* 속성값 */
+              ++cnt;
+              console.log(node); 
+           }
+    	}
   	  }
-    } else if(nodeInput != "") { // 노드명이 존재하는 경우
-  	  if(document.getElementById('idValue').value != "") { // id존재 o
-  		 if(node.localName == nodeInput) { /* id > 노드명 */
-  			++cnt;
-  		    name = "id >= 노드명 개수 = ";
-    		console.log(node);
-  		 } 
-  	  } else { // id존재 x
-  		 if(attrInput == "" && attrValInput == "" && node.localName == nodeInput) { // 노드명만 존재하는 경우
-    		name = "노드명 개수 = ";
+    } else if(nodeInput != "" && node.localName == nodeInput) { // 노드명(o)
+  	  if(idValue != "" && idValue == idSearch.id) { // id (o)
+  		 ++cnt;  /* id > 노드명 */
+  		 name = "id >= 노드명 개수 = ";
+    	 console.log(node);
+  	  } else if(idValue == ""){ // id (x)
+  		 if(attrInput == "" && attrValInput == "" && node.localName == nodeInput) { 
+    		name = "노드명 개수 = "; // id (x). 노드명(o), 속성명(x), 속성값(x)
      		++cnt;
     		console.log(node);
-    	  } else { // 현재node의 속성이 존재하는 경우
+    	  } else if(attrInput != "" || attrValInput != "") { // 노드명(o), 속성명 or 속성값 존재하는 경우
     	    for(var i = 0; i < node.attributes.length; i++) {
  	    	   if(node.attributes[i].nodeName == attrInput && node.attributes[i].nodeValue == attrValInput) { 
  	    		  name = "노드명 + 속성명 + 속성값 개수 = "; /* 노드 + 속성명 + 속성값 */
  	    		  ++cnt;
  		    	  console.log(node);
- 	    	   } else if(node.attributes[i].nodeValue == attrValInput) { /* 노드 + 속성값 */
- 	    		  name = "노드명 + 속성값 개수 = ";
+ 	    	   } else if(node.attributes[i].nodeName == attrInput && attrValInput == "") { 
+ 	    		  name = "노드명 + 속성명 개수 = "; /* 노드 + 속성명 */
  	    		  ++cnt;
  		    	  console.log(node);
- 	    	   } 
+ 		    	  
+ 	    	   } else if(node.attributes[i].nodeValue == attrValInput && attrInput == "") {
+ 	    		  name = "노드명 + 속성값 개수 = "; /* 노드 + 속성값 */
+ 	    		  ++cnt;
+ 		    	  console.log(node);
+ 	    	   }
      	    }
   		 }
   	  }
     }
   // 4. idSearchd의 자식node를 다 돌면 return  
-  if(node == idSearch && index >= idSearch.childElementCount) {
+  if(node == idSearch && index >= idSearch.childElementCount)
      return name + cnt;
-  }
   // 3-1. node의 형제 노드가 있다면 node의 형제 노드를 다 돈다.
   if(node.nextElementSibling != null && preNode != idSearch) {
     node = node.nextElementSibling;
