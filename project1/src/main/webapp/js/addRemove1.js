a2095cde81265098d182bd83baeb034fc9bc14a9
soyoungkim9 <soyoung930623@gmail.com>
2018-12-26 09:19:38 +0900

    Week 4 Wed - node add/remove success03(JavaScript) - 최종마무리 정리

---
+++ project1/src/main/webapp/js/addRemove1.js
@@ -0,0 +1,17 @@
+function add() {
+  var output = nodeInput.value;
+  if(output != "")
+    tb.innerHTML += output; 
+}
+
+function remove() {
+  if(tb.childElementCount == 0) {
+    tb.innerHTML = "더 이상 삭제할 노드가 없습니다.";
+  } else {
+      tb.removeChild(tb.lastElementChild);
+  }
+}
+
+document.getElementById('addBtn1').addEventListener('click', add);
+document.getElementById('removeBtn1').addEventListener('click', remove);
+
