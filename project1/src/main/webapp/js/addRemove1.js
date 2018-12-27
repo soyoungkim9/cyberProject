bc5c52c9db8c5cb96c6f91662e0594fd9ab7ee1a
soyoungkim9 <soyoung930623@gmail.com>
2018-12-27 21:22:25 +0900

    Week 4 Thur - node add/remove success04(JavaScript)

--- project1/src/main/webapp/js/addRemove1.js
+++ project1/src/main/webapp/js/addRemove1.js
@@ -1,17 +1,53 @@
+var tbody = document.getElementById('content');
+
 function add() {
-  var output = nodeInput.value;
-  if(output != "")
-    tb.innerHTML += output; 
+	var time = document.getElementById('time').value;
+	var price = document.getElementById('price').value;
+	var netChange = document.getElementById('netChange').value;
+	var sign = document.getElementById('sign').value;
+	var accumulate = document.getElementById('accumulate').value;
+
+	var tr, td, text;
+	tr = document.createElement("tr");
+	for (var i = 0; i < 5; i++) {
+		td = document.createElement("td");
+		switch (i) {
+		case 0:
+			text = document.createTextNode(time);
+			break;
+		case 1:
+			text = document.createTextNode(price);
+			break;
+		case 2:
+			text = document.createTextNode(netChange);
+			break;
+		case 3:
+			text = document.createTextNode(sign);
+			break;
+		case 4:
+			text = document.createTextNode(accumulate);
+			break;
+		}
+		td.appendChild(text);
+		tr.appendChild(td);
+
+		if (tbody.children.length <= 7) {
+			if (tbody.children.length == 0) {
+				tbody.appendChild(tr);
+			} else {
+				tbody.insertBefore(tr, tbody.childNodes[0]);
+			}
+		} else {
+			tbody.removeChild(tbody.lastElementChild);
+			tbody.insertBefore(tr, tbody.childNodes[0]);
+		}
+	}
 }
 
 function remove() {
-  if(tb.childElementCount == 0) {
-    tb.innerHTML = "더 이상 삭제할 노드가 없습니다.";
-  } else {
-      tb.removeChild(tb.lastElementChild);
-  }
+	if (tbody.children.length != 0)
+		tbody.removeChild(tbody.lastElementChild);
 }
 
 document.getElementById('addBtn1').addEventListener('click', add);
 document.getElementById('removeBtn1').addEventListener('click', remove);
-
